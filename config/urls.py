443e5d6d544604ebea387181338373ddf0489835
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-29 16:39:18 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from dashboard.views import dashboard
 
 urlpatterns = [
+    path("", dashboard),   # ⭐ 关键：根路径
     path("orders/", include("orders.urls")),
 ]

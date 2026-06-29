fd5e5d9f95452367fa3eefdeb3c0e0b8eaf34a83
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-29 11:54:41 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,7 @@
 from django.http import JsonResponse
-from orders.models import Order
 
-def dashboard_data(request):
-    qs = Order.objects.all()
-
+def dashboard(request):
     data = {
-        "total": qs.count(),
-        "eu": qs.filter(region='EU').count(),
-        "us": qs.filter(region='US').count(),
-        "ca": qs.filter(region='CA').count(),
-        "t12": qs.filter(created_hours__gte=12).count(),
-        "t24": qs.filter(created_hours__gte=24).count(),
-        "t36": qs.filter(created_hours__gte=36).count(),
-        "t48": qs.filter(created_hours__gte=48).count(),
+        "ok": True
     }
-
     return JsonResponse(data)

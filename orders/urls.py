b4a7985385d97be1b7e959397ca58122532dd95b
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-29 16:52:19 +0800

    Update order URLs to include dashboard and new views

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import orders_list, export_orders
+from . import views
 
 urlpatterns = [
-    path("list/", orders_list),
-    path("export/", export_orders),
+    path("dashboard/", views.dashboard),
+    path("list/", views.order_list),
+    path("export/", views.export_ops_orders),
 ]

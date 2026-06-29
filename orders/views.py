1134d0d24dad9fe71f3fd7c2398211c534dc6462
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-29 16:02:22 +0800

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -1,18 +1,17 @@
+from django.http import JsonResponse
+from .models import Order
 
-import csv
-from django.http import HttpResponse
-from orders.models import Order
+def orders_list(request):
+    qs = Order.objects.all().order_by("-id")
 
-def export_ops_orders(request):
-    qs = Order.objects.filter(created_hours__gte=12)
-
-    resp = HttpResponse(content_type='text/csv')
-    resp['Content-Disposition'] = 'attachment; filename=ops.csv'
-
-    w = csv.writer(resp)
-    w.writerow(['订单号','店铺','区域','小时','物流单号'])
-
+    data = []
     for o in qs:
-        w.writerow([o.order_no,o.shop_name,o.region,o.created_hours,o.logistics_no])
+        data.append({
+            "order_no": o.order_no,
+            "shop_name": o.shop_name,
+            "region": o.region,
+            "created_hours": o.created_hours,
+            "logistics_no": o.logistics_no,
+        })
 
-    return resp
+    return JsonResponse({"data": data})

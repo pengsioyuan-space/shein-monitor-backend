724c88d34b6bdc17b3d692a9421af85a12d97d3b
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-30 16:13:51 +0800

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -1,17 +1,9 @@
-# orders/models.py
+from django.db import models
+
 
 class DashboardStats(models.Model):
+    data = models.JSONField()
     updated_at = models.DateTimeField(auto_now=True)
 
-    total = models.IntegerField(default=0)
-    eu = models.IntegerField(default=0)
-    us = models.IntegerField(default=0)
-    ca = models.IntegerField(default=0)
-    other = models.IntegerField(default=0)
-
-    pending_process_risk = models.IntegerField(default=0)
-    pending_ship_risk = models.IntegerField(default=0)
-    pickup_appointment_risk = models.IntegerField(default=0)
-    pickup_risk = models.IntegerField(default=0)
-
-    data = models.JSONField(default=dict)
+    def __str__(self):
+        return f"DashboardStats {self.id}"

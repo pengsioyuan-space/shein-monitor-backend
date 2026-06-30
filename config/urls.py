6b568b1e85be77842750e116433484f82bc52367
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-30 16:11:27 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path, include
-from dashboard.views import dashboard
 
 urlpatterns = [
-    path("", dashboard),
-    path("dashboard/", dashboard),
     path("orders/", include("orders.urls")),
 ]

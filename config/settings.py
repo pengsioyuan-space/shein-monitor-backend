e2c00e77198edfea5874a084ea0585a4c2c14926
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-30 15:18:10 +0800

    Refactor settings to use environment variables
    Removed hardcoded settings and integrated dj_database_url for database configuration.

--- config/settings.py
+++ config/settings.py
@@ -1,31 +1,9 @@
-SECRET_KEY = 'dev'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-
-INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'orders.apps.OrdersConfig',
-    'dashboard.apps.DashboardConfig',
-]
+import os
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
-
-ROOT_URLCONF = 'config.urls'
-MIDDLEWARE = []
-
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [BASE_DIR if (BASE_DIR:=None) else ""],
-    'APP_DIRS': True,
-}]
-
-STATIC_URL = '/static/'

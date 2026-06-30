d84324a8a365ff9a3dc34fe1e80027620dffcd93
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-30 10:59:34 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from orders import views as order_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", order_views.dashboard),
+    path("dashboard/", order_views.dashboard),
     path("orders/", include("orders.urls")),
 ]

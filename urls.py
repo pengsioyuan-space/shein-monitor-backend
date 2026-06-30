89189952610fdd9845eba4c21355f8c598682aed
pengsioyuan-space <pengsioyuan@gmail.com>
2026-06-30 14:26:39 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,7 +4,10 @@ from orders import views as order_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # 根路径直接返回 dashboard，方便你打开 Railway 域名就看到数据
     path("", order_views.dashboard),
+
     path("dashboard/", order_views.dashboard),
     path("orders/", include("orders.urls")),
 ]
